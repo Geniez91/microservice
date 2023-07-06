83e8771f062944a3e2d6cca5a3ceb340c6ae5c5c
Jeremy Weltmann <jeremy.we@hotmail.fr>
2023-07-06 23:28:10 +0200

    add limit text-field

--- front/pathe/src/components/Movies.vue
+++ front/pathe/src/components/Movies.vue
@@ -13,14 +13,18 @@
          :subtitle=movie.realease_date
          :text="movie.synopsis"
          variant="tonal"
->
+> 
+
+<v-img src="https://fr.web.img2.acsta.net/pictures/23/06/07/14/33/5787419.jpg" aspect-ratio="1"  cover class="fill-height fill-width mb-2" ></v-img>
   <div>
     {{ movie.description }}
   </div>
   <hr>
   <div class="py-4">
-    <h4>Reserver vos Places</h4>
-    <v-row>
+    <h4 class="text-center"><v-icon size="x-large"> <v-img
+  :src="billet" class="mr-2"
+></v-img></v-icon>Reserver vos Places</h4>
+    <v-row v-if="movie.availableTickets>0">
     <v-col cols="9">
       <v-text-field
   v-model="numberValue"
@@ -28,14 +32,20 @@
   single-line
   type="number"
   label="Nombre de Places" 
+  :min="1"
+  :max="movie.availableTickets"
 />
+
 </v-col>
-<v-col cols="3">
+<v-col cols="3" style="display: flex;align-items: center;" >
 <v-btn @click="validerReservation(movie.idMovies, numberValue)">Ajouter</v-btn>
 </v-col>
   
   </v-row>
-
+  <div v-else class="py-3">
+<h4 class="text-grey text-center" ><v-icon size="x-large"> <v-img
+  :src="annuler" class="mr-2"></v-img> </v-icon>Aucune Place Disponible</h4>
+</div>
   
   </div>
 
@@ -53,11 +63,15 @@
 
 <script>
 import { getMovies,updateTicket } from '../services/movies.services';
+import billet from '../assets/billet.png'
+import annuler from '../assets/annuler.png'
 
 export default {
   data() {
     return {
-      movies: []
+      movies: [],
+      billet:billet,
+      annuler:annuler
     };
   },
   mounted() {
